770a1ef901dffd9d5dd721f0683707671925936b
rahul <singhrahul38202@gmail.com>
2026-01-05 22:49:37 +0530

    Time: 9 ms (6.67%) | Memory: 42.8 MB (6.67%) - LeetSync

--- 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.java
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.java
@@ -1,11 +1,23 @@
 class Solution {
     public int minimumNumbers(int num, int k) {
-        if(num==0) return 0;
-        for(int i =1;i<=10;i++){
-        if(i*k > num) break;
-        if(( i*k %10) == num%10)
-        return i;
+        if (num == 0) return 0;
+        int[] dp = new int[num + 1];
+        Arrays.fill(dp, -1);
+        int res = fun(num, k, dp);
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    public int fun(int num, int k, int[] dp) {
+        if (num < 0) return Integer.MAX_VALUE;
+        if (num == 0) return 0;
+        if (dp[num] != -1) return dp[num];
+        int ans = Integer.MAX_VALUE;
+        for (int val = k; val <= num; val += 10) {
+            if (val == 0) continue;
+            int sub = fun(num - val, k, dp);
+            if (sub != Integer.MAX_VALUE) {
+                ans = Math.min(ans, sub + 1);
+            }
         }
-        return -1;
+        return dp[num] = ans;
     }
 }
