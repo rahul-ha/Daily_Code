173dc127d196c0d3b4ff047df799ca6712c01e9f
rahul <singhrahul38202@gmail.com>
2026-01-29 16:53:49 +0530

    Time: 7 ms (92.68%) | Memory: 45.7 MB (73.10%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -1,23 +1,24 @@
+
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
         Arrays.sort(deck);
         int arr[] = new int[deck.length];
         Arrays.fill(arr,0);
-        int i =0;
-        int j = 0;
-
-        boolean skip = false;
-        while(i<deck.length){
-            if(arr[j]==0){
-                if(skip==false){
-                     arr[j] = deck[i];
-                i++;
-                }
-                skip = !skip;
-            }
-           
-            j = (j+1)%deck.length;
+       Deque<Integer> dq = new ArrayDeque<>();
+       for(int i =0;i<deck.length;i++){
+        dq.add(i);
+       }
+       int i =0;
+       while(i<deck.length){
+        int a = dq.pollFirst();
+        arr[a] = deck[i];
+        if(!dq.isEmpty()){
+               int b  = dq.pollFirst();
+        dq.addLast(b);
         }
-        return arr;
-    }
-}
+     
+        i++;
+       }
+
+      return arr;
+}}
