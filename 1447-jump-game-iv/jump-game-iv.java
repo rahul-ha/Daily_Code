30756af32b84ac47447c1399cab7f0055e5834f7
rahul <singhrahul38202@gmail.com>
2026-01-30 12:22:57 +0530

    Time: 70 ms (30.63%) | Memory: 98.1 MB (23.59%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+      //  if(arr.length==1) return 0;
+        for(int i =0;i<arr.length;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],new ArrayList<>());
+            }
+            map.get(arr[i]).add(i);
+        }
+       
+        Queue<Integer> q = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        int step = 0;
+         q.add(0);
+        visited.add(0);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size-->0){
+                int n = q.poll();
+                if(n== arr.length-1)
+                return step;
+                if(n-1>=0 && !visited.contains(n-1)){
+                    visited.add(n-1);
+                    q.add(n-1);
+                }
+
+                if(n+1< arr.length && !visited.contains(n+1)){
+                      visited.add(n+1);
+                    q.add(n+1);
+                }
+
+                if(map.containsKey(arr[n])){
+                    for(int l : map.get(arr[n])){
+                        if(!visited.contains(l)){
+                            visited.add(l);
+                            q.add(l);
+                        }
+                    }
+                }
+                map.remove(arr[n]);
+            }
+            step++;
+        }
+
+        return -1;
+    }
+}
