cbe4170c39e16c31ef12272ee26333f063146f43
rahul <singhrahul38202@gmail.com>
2026-04-14 12:18:22 +0530

    Time: 313 ms (26.50%) | Memory: 93.3 MB (64.68%) - LeetSync

---
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count =0;
+        for(int i =0;i<nums.length;i++){
+            int n = nums[i];
+            boolean a = true;
+            while(a){
+                if(i%2==0 && check(n)){
+                    a = false;
+                }
+                else if(i%2!=0 && !check(n)){
+                    a = false;
+                }
+                else{
+                    n++;
+                    count++;
+                }
+            }
+        }
+            return count;
+        }
+
+        public boolean check(int n){
+            if(n<=1) return false;
+            if(n==2) return true;
+            for(int i =2;i*i<=n;i++){
+                if(n%i==0) return false;
+            }   
+            return true;         
+        }
+
+    
+}
