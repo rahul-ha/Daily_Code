5109c0c0a587c5476e925f8ffe9e45122c78c893
rahul <singhrahul38202@gmail.com>
2026-07-31 04:13:56 +0530

    Time: 18 ms (23.41%) | Memory: 48 MB (73.70%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -8,18 +8,7 @@ class Solution {
         int ans = 0;
         Arrays.sort(arr, Collections.reverseOrder());
         for(int i =0;i<26;i++){
-            if(i<=7){
-                ans = ans+ arr[i];
-            }
-            else if (i>7&& i<16){
-                ans = ans+ (arr[i]*2);
-            }
-            else if(i>=16 && i<=23){
-                ans =  ans+(arr[i]*3);
-            }
-            else {
-                ans =  ans+(arr[i]*4);
-            }
+           ans = ans + (arr[i]*((i/8)+1));
         }
         return ans;
 
