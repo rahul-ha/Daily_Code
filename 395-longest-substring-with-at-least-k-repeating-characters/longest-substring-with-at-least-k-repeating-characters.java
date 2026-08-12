cb6fc332333549a7a67f2a253681f65088ad0bff
rahul <singhrahul38202@gmail.com>
2026-08-13 01:02:06 +0530

    Time: 359 ms (10.06%) | Memory: 43.9 MB (41.98%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -6,26 +6,23 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
 
             int[] freq = new int[26];
-            int invalid = 0;
 
             for (int j = i; j < s.length(); j++) {
 
                 int idx = s.charAt(j) - 'a';
-
-                // Before increasing frequency
-                if (freq[idx] > 0 && freq[idx] < k) {
-                    invalid--;
-                }
-
                 freq[idx]++;
 
-                // After increasing frequency
-                if (freq[idx] < k) {
-                    invalid++;
+                // Check whether current substring is valid
+                boolean valid = true;
+
+                for (int x = 0; x < 26; x++) {
+                    if (freq[x] > 0 && freq[x] < k) {
+                        valid = false;
+                        break;
+                    }
                 }
 
-                // Every character has frequency >= k
-                if (invalid == 0) {
+                if (valid) {
                     ans = Math.max(ans, j - i + 1);
                 }
             }
