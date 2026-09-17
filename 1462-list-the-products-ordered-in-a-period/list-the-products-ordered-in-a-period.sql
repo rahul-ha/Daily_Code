8a95bd8d34add384a13a464a86a28ca5b079f748
rahul <singhrahul38202@gmail.com>
2026-09-17 23:38:31 +0530

    Time: 973 ms (21.30%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-SELECT p.product_name, SUM(o.unit) AS unit
-FROM Products p
-JOIN Orders o
-ON p.product_id = o.product_id
-WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY p.product_id
-HAVING SUM(o.unit) >= 100;
+select p.product_name , sum(o.unit) as unit from Products as p join Orders as o on
+p.product_id = o.product_id where o.order_date between '2020-02-01' and '2020-02-29'
+group by p.product_id having sum(o.unit)>=100;
